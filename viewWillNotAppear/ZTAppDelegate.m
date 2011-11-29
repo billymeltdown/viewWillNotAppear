eed676758f84b32ec6f148e1dfd3a32a80b9d7d3
Stephen Lombardo <sjlombardo@zetetic.net>
2011-11-29 11:42:47 -0500

    hide tab view on resign active, popup login view when app becomes active

--- viewWillNotAppear/ZTAppDelegate.m
+++ viewWillNotAppear/ZTAppDelegate.m
@@ -54,8 +54,7 @@
     // reveal the application window now that we're all set
     [self.window setHidden:NO];
     
-    // present the login view modally to hide the app view
-    [self _hideAppView:NO];
+    self.tabBarController.view.hidden = YES;
     
     return YES;
 }
@@ -66,7 +65,7 @@
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-    [self _hideAppView:YES];
+    self.tabBarController.view.hidden = YES;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -89,6 +88,7 @@
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+    [self _hideAppView:YES];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
@@ -104,6 +104,7 @@
 {
     //[[self tabBarController] dismissModalViewControllerAnimated: animated];
     [[self tabBarController] dismissViewControllerAnimated: animated completion:NULL];
+    self.tabBarController.view.hidden = NO;
 }
 
 - (void)_hideAppView:(BOOL)animated
