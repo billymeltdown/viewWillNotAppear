1ba91922ab8ba55addabee391f19da402e640f86
Billy Gray <wgray@zetetic.net>
2011-11-29 12:15:57 -0500

    looks nicer sans animated presentation

--- viewWillNotAppear/ZTAppDelegate.m
+++ viewWillNotAppear/ZTAppDelegate.m
@@ -90,7 +90,7 @@
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
-    [self _hideAppView:YES];
+    [self _hideAppView:NO];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
